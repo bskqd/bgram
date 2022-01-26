74268e4afe9e10427e4fdedd7714ec7661934a82
Fagtoy <bodiakonovalenko@gmail.com>
2022-01-26 22:25:39 +0200

    refactor messages services

--- project/chat/services/chat.py
+++ project/chat/services/chat.py
@@ -1,10 +1,17 @@
 from collections import defaultdict
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Optional, Iterator, Union
 
-from starlette.websockets import WebSocket
+from fastapi import WebSocket
+from sqlalchemy import select, delete
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import Select
 
 from accounts.models import User
+from chat.constants.messages import MessagesActionTypeEnum
+from chat.models import Message
+from chat.schemas.messages import ListMessagesSchema
+from mixins.services.crud import CRUDOperationsService
 
 
 class WebSocketConnection:
@@ -38,3 +45,61 @@ class ChatRoomsWebSocketConnectionManager:
 
 
 chat_rooms_websocket_manager = ChatRoomsWebSocketConnectionManager()
+
+
+class MessagesService:
+    def __init__(
+            self,
+            db_session: Session,
+            chat_room_id: Optional[int] = None,
+            broadcast_action_to_chat_room: bool = True
+    ):
+        self.db_session = db_session
+        self.chat_room_id = chat_room_id
+        self.broadcast_action_to_chat_room = broadcast_action_to_chat_room
+
+    async def list_messages(
+            self,
+            chat_room_id: Optional[int] = None,
+            queryset: Select = select(Message)
+    ) -> Iterator[Message]:
+        if chat_room_id:
+            queryset = queryset.where(Message.chat_room_id == chat_room_id)
+        messages = await self.db_session.scalars(queryset)
+        return messages.all()
+
+    async def create_message(self, chat_room_id: int, text: str, author_id: Optional[int] = None, **kwargs) -> Message:
+        message = Message(chat_room_id=chat_room_id, text=text, author_id=author_id, **kwargs)
+        created_message = await CRUDOperationsService(self.db_session).create_object_in_database(message)
+        if self.broadcast_action_to_chat_room:
+            await self._broadcast_action_to_chat_room(
+                chat_room_id, MessagesActionTypeEnum.CREATED.value, ListMessagesSchema.from_orm(created_message).dict(),
+            )
+        return created_message
+
+    async def update_message(self, message: Union[int, Message], **kwargs) -> Message:
+        crud_service = CRUDOperationsService(self.db_session)
+        if isinstance(message, int):
+            message: Message = await crud_service.get_object(select(Message), Message, message)
+        kwargs['is_edited'] = True
+        updated_message = await crud_service.update_object_in_database(message, **kwargs)
+        if self.broadcast_action_to_chat_room:
+            await self._broadcast_action_to_chat_room(
+                self.chat_room_id, MessagesActionTypeEnum.UPDATED.value,
+                ListMessagesSchema.from_orm(updated_message).dict(),
+            )
+        return updated_message
+
+    async def delete_message(self, message_id: int) -> int:
+        await self.db_session.execute(delete(Message).where(Message.id == message_id))
+        if self.broadcast_action_to_chat_room:
+            await self._broadcast_action_to_chat_room(
+                self.chat_room_id, MessagesActionTypeEnum.DELETED.value, {'message_id': message_id},
+            )
+        return message_id
+
+    async def _broadcast_action_to_chat_room(self, chat_room_id: int, action: str, message_data: dict):
+        await chat_rooms_websocket_manager.broadcast(
+            message={'action': action, **message_data},
+            chat_room_id=chat_room_id
+        )
