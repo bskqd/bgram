5f51e07af0f9364bba721c385bf388fedfd5e91a
Fagtoy <bodiakonovalenko@gmail.com>
2022-04-10 23:43:11 +0300

    fix docs/ generation

--- project/mixins/views.py
+++ project/mixins/views.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Optional, Type
+from typing import Optional
 
 from fastapi import Request, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,7 +8,6 @@ from sqlalchemy.sql import Select
 from accounts.models import User
 from database.repository import BaseCRUDRepository
 from mixins import dependencies as mixins_dependencies
-from mixins.filters import FilterSet
 
 
 class AbstractView(ABC):
@@ -20,7 +19,7 @@ class AbstractView(ABC):
     db_session: AsyncSession = Depends(mixins_dependencies.db_session)
     db_query = None
     pagination_class = None
-    filterset_class: Optional[Type[FilterSet]] = None
+    filterset_class = None
 
     def get_db_query(self, *args) -> Select:
         if self.db_query is not None:
