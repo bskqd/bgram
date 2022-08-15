7c2f72e668713981dc6fc940f5e226fdefeb161d
bohdankonovalenko <bodiakonovalenko@gmail.com>
2022-08-16 00:18:58 +0300

    initially added celery app to the project

--- project/core/celery/celery_app.py
+++ project/core/celery/celery_app.py
@@ -7,5 +7,5 @@ load_dotenv()
 
 
 celery = Celery('bgram_celery_app')
-celery.conf.broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/1')
-celery.conf.result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/2')
+celery.conf.broker_url = os.getenv('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/1')
+celery.conf.result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/2')
