30a06fa9145fc7e40d7b5755679730fe07bb816b
bohdankonovalenko <bodiakonovalenko@gmail.com>
2022-08-16 23:29:12 +0300

    added celery completely

--- project/core/celery/celery_app.py
+++ project/core/celery/celery_app.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-celery = Celery('bgram_celery_app')
-celery.conf.broker_url = os.getenv('CELERY_BROKER_URL', 'redis://127.0.0.1:6379/1')
-celery.conf.result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://127.0.0.1:6379/2')
+bgram_celery_app = Celery(
+    'bgram_celery_app',
+    broker=os.getenv('CELERY_BROKER_URL'),
+    backend=os.getenv('CELERY_BROKER_URL'),
+)
