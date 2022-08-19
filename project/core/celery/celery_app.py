3cec71583f3552fa0052955f03c2065ac9ec8424
bohdankonovalenko <bodiakonovalenko@gmail.com>
2022-08-19 22:22:36 +0300

    added celery beat service

--- project/core/celery/celery_app.py
+++ project/core/celery/celery_app.py
@@ -8,3 +8,8 @@ bgram_celery_app = Celery('bgram_celery_app')
 bgram_celery_app.config_from_object('core.celery.celeryconfig')
 
 bgram_celery_app.autodiscover_tasks(['core.celery'])
+
+
+@bgram_celery_app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    pass
