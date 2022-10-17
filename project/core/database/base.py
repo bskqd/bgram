bc8cd0baa6592663039abfc05d2303686e37b2e2
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2022-10-17 23:27:54 +0300

    Update base.py

--- project/core/database/base.py
+++ project/core/database/base.py
@@ -6,11 +6,6 @@ from core.dependencies.providers import provide_settings
 
 engine = create_async_engine(provide_settings().DATABASE_URL)
 
-
-async def create_sessionmaker() -> sessionmaker:
-    return sessionmaker(engine, autoflush=False, expire_on_commit=False, class_=AsyncSession)
-
-
 DatabaseSession = sessionmaker(engine, autoflush=False, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
